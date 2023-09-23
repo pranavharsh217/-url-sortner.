7d758b11849e368a69422c2604251c6ff29ae445
PRANAV SINHA <93813401+pranavharsh217@users.noreply.github.com>
2023-09-23 16:14:52 +0530

    Add files via upload

---
+++ shortUrl.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const shortId = require('shortid')
+
+const shortUrlSchema = new mongoose.Schema({
+  full: {
+    type: String,
+    required: true
+  },
+//   hfxghjk
+  short: {
+    type: String,
+    required: true,
+    default: shortId.generate
+  },
+  clicks: {
+    type: Number,
+    required: true,
+    default: 0
+  }
+})
+// THIS IS A CHANGE IN THIS REPO
+
+module.exports = mongoose.model('ShortUrl', shortUrlSchema);
